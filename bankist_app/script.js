0a0e231ccd3cdcab970cb26f83c2c43566b2eec0
md <mdmonish143@gmail.com>
2023-08-01 16:31:35 +0530

    section animation shows hidden on scroll added

--- bankist_app/script.js
+++ bankist_app/script.js
@@ -153,7 +153,7 @@ const navHeight = nav.getBoundingClientRect().height;
 
 const callBackOb = function(entries){
 const [entry]= entries;
-console.log(entry)
+
 if(!entry.isIntersecting)nav.classList.add('sticky')
 else nav.classList.remove('sticky')
 }
@@ -167,7 +167,28 @@ const optionsOb = {
 const observer = new IntersectionObserver(callBackOb,optionsOb);
 observer.observe(header);
 
+//section animation on scroll
 
+const allSections =document.querySelectorAll('.section');
+
+const revealSection = function(entries,observer){
+  const [entry] = entries;
+  console.log("enside",entry.target.classList)
+  if(!entry.isIntersecting)return;
+
+  entry.target.classList.remove('section--hidden')
+  observer.unobserve(entry.target)
+}
+
+const sectionObserver = new IntersectionObserver(revealSection,{
+  root:null,
+  threshold:0.15,
+});
+
+allSections.forEach(function(section){
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden')
+})
 
 
 //lect
